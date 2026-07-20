947fb9c37713dae0d03d02b04a2028db6d8b76b4
Neftalem <dneftalem@gmail.com>
2026-07-20 11:26:14 +0300

    feat: import db on server.js and check db connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const db = require('./config/db')
 const cors = require('cors');
 
 const { notFound, errorHandler } = require('./middleware/errorHandler');
