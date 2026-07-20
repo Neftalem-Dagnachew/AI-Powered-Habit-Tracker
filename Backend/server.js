0a4759b9bc2a749a0a6b503349103d014c89daec
Neftalem <dneftalem@gmail.com>
2026-07-20 10:59:45 +0300

    Feat: implement main server config with .env

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,23 @@
-require('dotenv').config({ path: '/.env' });
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const { notFound, errorHandler } = require('./middleware/errorHandler');
+
 const app = express();
 
-// express use
+// Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Route
+
+// Error Handling Middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
