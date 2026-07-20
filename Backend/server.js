f26d3b5bef7c56bbff182f7dce34a8f50896b583
Neftalem <dneftalem@gmail.com>
2026-07-20 10:01:08 +0300

    Feat: add new file server.js & errorHandler.js

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config({ path: '/.env' });
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// express use
+app.use(cors());
+app.use(express.json());
+
+// Route
