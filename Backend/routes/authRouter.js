e4b5ab2d5919ec9d3b9bd56826f67c9f198308b2
Neftalem <dneftalem@gmail.com>
2026-07-21 18:24:43 +0300

    Feat: create router and add protect

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, login } = require('../controllers/authController');
-
+const { registerUser, login, getMe } = require('../controllers/authController');
+const { protect } = require('../middleware/auth');
 // (POST /api/auth/register)
 router.post('/register', registerUser);
 
 // (POST /api/auth/login)
 router.post('/login', login);
+
+router.get('/me', protect, getMe)
 
 module.exports = router;
