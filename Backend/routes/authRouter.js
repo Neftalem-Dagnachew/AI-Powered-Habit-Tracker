ac6f7deb5e8fee9404eafd19f985da7b92f5d9cf
Neftalem <dneftalem@gmail.com>
2026-07-21 19:30:28 +0300

    feat: add create router for updateProfile

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, login, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
+const { updateProfile } = require('../models/user');
 // (POST /api/auth/register)
 router.post('/register', registerUser);
 
 // (POST /api/auth/login)
 router.post('/login', login);
 
-router.get('/me', protect, getMe)
+router.get('/me', protect, getMe);
+
+router.get('/profile', protect, updateProfile);
 
 module.exports = router;
