081706be7f87e326127c0be641955b00c939bf92
Neftalem <dneftalem@gmail.com>
2026-08-01 12:32:05 +0300

    add darck mode landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,1 +1,12 @@
 import {Routes, Route} from "react-router-dom"
+import Landing from "./Pages/Landing"
+
+function App(){
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />}/>
+    </Routes>
+  )
+}
+
+export default App;
